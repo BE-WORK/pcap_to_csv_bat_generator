3f2b0164ab57b60ef0e1647d92b6e0b2cbf6e873
liuhuisen <liuhuisen@hotmail.com>
2018-01-03 21:08:25 +0800

    Add @echo off to supress the print of command itself.

--- pcap_to_csv_bat_generator.py
+++ pcap_to_csv_bat_generator.py
@@ -20,6 +20,8 @@ def pcap_to_csv_bat_generator(folder_path):
     # 提取目标字段至csv文件
     file_list = os.listdir(folder_path + 'sample_traffic_pcap/')
     bat_file = open('pcap_to_csv.bat', 'w')
+    bat_file.write('@echo off')
+    bat_file.write('\n')
     for file_name in file_list:
         bat_file.write(
             'tshark -r ' + folder_path + 'sample_traffic_pcap/' + file_name + ' -Y "(ip)&&(tcp)" -T fields -e _ws.col.No. -e _ws.col.Time -e _ws.col.Source -e _ws.col.Destination -e _ws.col.Protocol -e _ws.col.Length -e _ws.col.Info -E header=y -E separator=, -E quote=d -E occurrence=f > ' + folder_path + 'sample_traffic_csv/' +
